30ce2ba1e547d7c02a8aa75479e66240f0b55faf
cbaba20 <cbaba20@gmail.com>
2018-04-15 22:11:46 +0530

    Add project files.

---
+++ LoginApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using LoginApplication.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApplication.Controllers
+{
+    //[SessionExpire]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginApplication/Models/AccountViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginApplication.Models
+{
+    
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name ="Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
